47a4a1b9c32c38a62ebb80ab8c822860fb367392
sidh1018 <f20241018@pilani.bits-pilani.ac.in>
2026-08-29 18:10:14 +0000

    lab01 task3

--- labs/lab01/task3/FA_GATE.v
+++ labs/lab01/task3/FA_GATE.v
@@ -4,10 +4,9 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  // Same five gates as the original full adder, deliberately reordered.
-  xor #(5) (ps, a, b);
-  and #(5) (pc2, cin, ps);
-  xor #(5) (sum, cin, ps);
-  and #(5) (pc1, a, b);
-  or  #(5) (cout, pc1, pc2);
+  xor #(2) (ps, a, b);
+  and #(2) (pc1, a, b);
+  xor #(2) (sum, cin, ps);
+  and #(2) (pc2, cin, ps);
+  or  #(2) (cout, pc1, pc2);
 endmodule
